4b4c01373152c093fa763733da83bde08feed649
sathira <sathira@minthrm.com>
2020-07-03 20:57:29 +0530

    near the dash borad

--- app/src/main/java/lk/sliit/sems_app/dash.java
+++ app/src/main/java/lk/sliit/sems_app/dash.java
@@ -1,5 +1,6 @@
 package lk.sliit.sems_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,12 +8,21 @@ import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class dash extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();;
-    private String email;
-    private String name;
+    public FirebaseDatabase firebaseDatabase;
+    public DatabaseReference databaseReference;
+    public DatabaseReference databaseReferenceUnits;
+    public String email;
+    public String name;
+    public String uid;
     public FirebaseAuth mAuth;
 
     @Override
@@ -25,6 +35,25 @@ public class dash extends AppCompatActivity {
         assert user != null;
         email=user.getEmail();
         name=user.getDisplayName();
+        uid=user.getUid();
+
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference(uid);
+        databaseReferenceUnits = databaseReference.child("Units");
+        databaseReferenceUnits.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String units = dataSnapshot.getValue(String.class);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+
 
         Log.d(TAG, "firebaseAuthWithGoogle:" + email);
     }
