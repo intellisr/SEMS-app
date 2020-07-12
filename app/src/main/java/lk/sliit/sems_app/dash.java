06879e2048b3b9f191858581ac79e0f2cdf16ce2
sathira <sathira@minthrm.com>
2020-07-12 22:53:14 +0530

    last 50

--- app/src/main/java/lk/sliit/sems_app/dash.java
+++ app/src/main/java/lk/sliit/sems_app/dash.java
@@ -3,9 +3,14 @@ package lk.sliit.sems_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,31 +27,55 @@ public class dash extends AppCompatActivity {
     public FirebaseDatabase firebaseDatabase;
     public DatabaseReference databaseReference;
     public DatabaseReference databaseReferenceUnits;
-    public String email;
+    public String proPic;
     public String name;
     public String uid;
     public FirebaseAuth mAuth;
+    private TextView uni1;
+    private TextView uni2;
+    private TextView uni3;
+    private TextView uname;
+    private ImageView userPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
 
+        uni1=findViewById(R.id.unit1);
+        uni2=findViewById(R.id.unit2);
+        uni3=findViewById(R.id.unit3);
+        uname=findViewById(R.id.name);
+        userPic=(ImageView)findViewById(R.id.upic);
+
+
         mAuth= FirebaseAuth.getInstance();
         FirebaseUser user=mAuth.getCurrentUser();
         assert user != null;
-        email=user.getEmail();
+        proPic = user.getPhotoUrl().toString();
         name=user.getDisplayName();
         uid=user.getUid();
 
+        uname.setText(name);
+        Glide.with(this).load(proPic).into(userPic);
+        Glide.with(getApplicationContext()).load(proPic)
+                .thumbnail(0.5f)
+                .crossFade()
+                .into(userPic);
+
+
         firebaseDatabase=FirebaseDatabase.getInstance();
-        databaseReference=firebaseDatabase.getReference(uid);
-        databaseReferenceUnits = databaseReference.child("Units");
+        databaseReference=firebaseDatabase.getReference("Units");
+        databaseReferenceUnits = databaseReference.child(uid);
         databaseReferenceUnits.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                JSONObject units = dataSnapshot.getValue(JSONObject.class);
+                live units = dataSnapshot.getValue(live.class);
+                Log.d(TAG, "units:" + units.getCol1());
 
+                uni1.setText("Unit1 : " + Long.toString(units.getCol7()));
+                uni2.setText("Unit2 : " +Long.toString(units.getCol8()));
+                uni3.setText("Unit3 : " +Long.toString(units.getCol9()));
             }
 
             @Override
@@ -56,7 +85,5 @@ public class dash extends AppCompatActivity {
 
         });
 
-
-        Log.d(TAG, "firebaseAuthWithGoogle:" + email);
     }
 }
---
+++ app/src/main/java/lk/sliit/sems_app/live.java
@@ -0,0 +1,70 @@
+package lk.sliit.sems_app;
+
+public class live {
+
+    public String col1;
+    public long col2;
+    public long col3;
+    public long col4;
+    public long col5;
+    public long col6;
+    public long col7;
+    public long col8;
+    public long col9;
+    public String col10;
+
+    public live(){}
+
+    public live(String col1, long col2, long col3, long col4, long col5, long col6, long col7, long col8, long col9, String col10) {
+        this.col1 = col1;
+        this.col2 = col2;
+        this.col3 = col3;
+        this.col4 = col4;
+        this.col5 = col5;
+        this.col6 = col6;
+        this.col7 = col7;
+        this.col8 = col8;
+        this.col9 = col9;
+        this.col10 = col10;
+    }
+
+    public String getCol1() {
+        return col1;
+    }
+
+    public long getCol2() {
+        return col2;
+    }
+
+    public long getCol3() {
+        return col3;
+    }
+
+    public long getCol4() {
+        return col4;
+    }
+
+    public long getCol5() {
+        return col5;
+    }
+
+    public long getCol6() {
+        return col6;
+    }
+
+    public long getCol7() {
+        return col7;
+    }
+
+    public long getCol8() {
+        return col8;
+    }
+
+    public long getCol9() {
+        return col9;
+    }
+
+    public String getCol10() {
+        return col10;
+    }
+}
